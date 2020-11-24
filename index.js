af0b9cd8b8a6dd3414210652e732138d0e41f178
lucas martinez <lucas.martinez@evoy.io>
2020-11-24 17:00:36 -0300

    atualizacao

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ const main = async () => {
    let resultadoDosAmbientes = [];
 
    listaDeAmbientes = [
-      "https://poc.relogtechnology.com/binpallet/",
-      "https://poc.relogtechnology.com/ferrolene/",
-      "https://bosch.relogtechnology.com/",
-      "https://aligntech.relogtechnology.com/",
-      "https://transnovag.relogtechnology.com/",
-      "https://gm.relogtechnology.com/",
-      "https://relogtechnology.com/",
+      "https://poc.relogtechnology.com/itauvip/",
+      // "https://poc.relogtechnology.com/binpallet/",
+      // "https://poc.relogtechnology.com/ferrolene/",
+      // "https://bosch.relogtechnology.com/",
+      // "https://aligntech.relogtechnology.com/",
+      // "https://transnovag.relogtechnology.com/",
+      // "https://gm.relogtechnology.com/",
+      // "https://relogtechnology.com/",
 
    ];
 
    listaDeCredenciais = [
       { email: "admin@admin.smart", password: "Admin20" },
-      { email: "admin@admin.smart", password: "Admin20" },
-      { email: "admin@admin.smart", password: "Admin20" },
-      { email: "admin@admin.smart", password: "Admin20" },
-      { email: "admin@admin.smart", password: "Admin20" },
-      { email: "admin@admin.smart", password: "Admin20" },
-      { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
+      // { email: "admin@admin.smart", password: "Admin20" },
    ];
 
    let nomeArquivos = [
-       "Binpallet.csv", "ferrolene.csv", "Bosch.csv", "Aligntech.csv", "Transnovag.csv", "Gm.csv", "Cebrace.csv"
+     "Binpallet.csv", "ferrolene.csv", "Bosch.csv", "Aligntech.csv", "Transnovag.csv", "Gm.csv", "Cebrace.csv"
    ]
 
    for (const [indice, baseUrl] of listaDeAmbientes.entries()) {
